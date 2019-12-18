eea31e7cf14fa36e1acdf2d9037e13901a554514
Ron3 <luodm03@qq.com>
2019-12-18 17:13:17 +0800

    -change: 加上环境变量.

--- bptest/test_read.py
+++ bptest/test_read.py
@@ -5,8 +5,10 @@ Create On 2019-12-18
 @author: Ron2
 """
 
-import pymongo
+import sys, os
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
+import pymongo
 from bptest import conf
 from bptest import common
 
