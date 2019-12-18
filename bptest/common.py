53ecbc6ace08ba730637a005c1a4e25f5ffc88e4
Ron3 <luodm03@qq.com>
2019-12-18 17:35:18 +0800

    -change: 修改好验证ok后的.

--- bptest/common.py
+++ bptest/common.py
@@ -15,8 +15,8 @@ def createDBConnection() -> (pymongo.mongo_client or None):
     :return:
     """
 
-    uri = "mongodb://{username}:{password}@{host}:{port}/{db_name}?authMechanism=SCRAM-SHA-256".format(username=conf.DB_USER,
-                                                                                                    password=conf.DB_PORT,
+    uri = "mongodb://{username}:{password}@{host}:{port}/?authSource={db_name}&authMechanism=SCRAM-SHA-256".format(username=conf.DB_USER,
+                                                                                                    password=conf.DB_PWD,
                                                                                                     host=conf.DB_HOST,
                                                                                                     port=conf.DB_PORT,
                                                                                                     db_name=conf.DB_NAME)
