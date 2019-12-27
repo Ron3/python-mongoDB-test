70d1c5aaf022331e5fcea7d2be35372c548a41a8
Ron3 <luodm03@qq.com>
2019-12-27 14:56:07 +0800

    -change: 加上本机随机读写/更新的. 1, 写入10W条指定_id, 本机耗费35秒 2, 随机读写1K条记录, 本机耗时0.39秒 3, 按照有索引的peopleId作为key, 本机耗费时0.45秒.

--- bptest/test_read.py
+++ bptest/test_read.py
@@ -8,9 +8,12 @@ Create On 2019-12-18
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
 
+import time
+import random
 import pymongo
 from bptest import conf
 from bptest import common
+
 
 
 class TestRead(object):
@@ -18,9 +21,13 @@ class TestRead(object):
     测试MongoDB的读取
     """
     def __init__(self):
-        self.client = common.createDBConnection()
-        self.db = self.client[conf.DB_NAME]
-        self.col = self.db["rontest"]
+        self.client = common.createDBConnection_defaultDB()
+        self.db = self.client[conf.USER_DB_NAME]
+        self.collection = self.db[conf.USER_DB_BP_USER_COLLECTION]
+        self.maxId = ""
+
+        self.phoneNum = 13000000000
+        self.peopleNum = 100000000000000000
 
 
     def start(self):
@@ -28,9 +35,53 @@ class TestRead(object):
         开始测试
         :return:
         """
-        result = self.col.find()
-        for doc in result:
-            print(doc)
+        # 测试修改修改名字
+        # ar = []
+        # beginTime = time.time()
+        # for x in range(1000):
+        #     peopleId = self.peopleNum + random.randint(12000, 91000)
+        #     peopleId = str(peopleId)
+        #     dic = self.collection.find_one({"peopleId": peopleId})
+        #     ar.append(dic.get("peopleId"))
+        #
+        #     # print("dic==>", dic)
+        # print("costTime ==> ", time.time() - beginTime)
+        #
+        # print(ar)
+
+        # ar = []
+        # beginTime = time.time()
+        # for x in range(1):
+        #     peopleId = self.peopleNum + random.randint(12000, 91000)
+        #     peopleId = str(peopleId)
+        #     dic = self.collection.find_one({"peopleId": peopleId})
+        #     ar.append(dic.get("peopleId"))
+        #     # self.collection.find_one_and_update(filter={"peopleId": peopleId}, update={"name": "化工局"})
+        #
+        #     # self.collection.find_one_and_replace()
+        #
+        #     # print("dic==>", dic)
+        # print("costTime ==> ", time.time() - beginTime)
+        #
+        # print(ar)
+
+        peopleId = str(self.peopleNum+1)
+        a = self.collection.find_one_and_update(filter={"peopleId": peopleId}, update={'$set': {"name": "化工局"}})
+        print("a ==> ", a, type(a))
+
+        a = self.collection.find_one({"peopleId": peopleId})
+        print("a ==> ", a, type(a))
+
+        # countNum = 0
+        # result = self.collection.find().limit(5)
+        # result.count()
+        # for dic in result:
+        #     countNum += 1
+        #
+        # print("countNum ==> ", countNum)
+
+
+
 
 
 
--- bptest/test_write.py
+++ bptest/test_write.py
@@ -9,6 +9,8 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
 
 import traceback
+import time
+import datetime
 
 
 from bptest import common
@@ -27,16 +29,54 @@ class TestWrite(object):
         self.collection = self.db[conf.USER_DB_BP_USER_COLLECTION]
         self.maxId = ""
 
+        self.phoneNum = 13000000000
+        self.peopleNum = 100000000000000000
+
+
 
     def _ReadMaxId(self):
         """
         读取最大的Id
         :return:
         """
-        maxIdDic = self.collection.find_one({"peopleId": {"$exists": True}}, sort=[("_id", -1)])
+        maxIdDic = self.collection.find_one({"bpId": {"$exists": True}}, sort=[("bpId", -1)])
+        if maxIdDic == None:
+            return 0
+
         print ("maxIdDic ==> ", maxIdDic)
-        peopleId = maxIdDic.get("peopleId")
-        return peopleId
+        bpId = maxIdDic.get("bpId")
+        return bpId
+
+
+    def createRegisterJson(self):
+        """
+        创建一个注册用户
+        :return:
+        """
+        self.phoneNum += 1
+        self.peopleNum += 1
+
+        dic = {}
+        dic["_id"] = str(self.phoneNum)
+        dic["bpId"] = int(time.time() * 1000)
+        dic["peopleId"] = str(self.peopleNum)
+        dic["name"] = "化以来"
+        dic["pwd"] = "ddaskjf1231"
+        dic["registerTimestamp"] = int(time.time())
+        # dic["registerDate"] = datetime.date.today()
+        # dic["registerTime"] = datetime.datetime.now().time()
+        dic["registerDate"] = "2019-12-27"
+        dic["registerTime"] = "11:36:06"
+        dic["idfa"] = "0000-0000-0000-0000"
+        dic["gameId"] = 1
+        dic["clientId"] = 12
+        dic["ip"] = "202.123.123.123"
+        dic["device"] = "MI 8 SE"
+        dic["accountType"] = 1
+        dic["lastLoginTime"] = int(time.time())
+        dic["lastPlayerGameId"] = 1
+        return dic
+
 
 
     def start(self):
@@ -50,10 +90,25 @@ class TestWrite(object):
         # # self.doc.insert({"_id": self.maxId, "title": "Ron"})
         # # self.doc.insert({"_id": 0, "maxId": self.maxId})
         # self.collection.replace_one(filter={"_id": self.maxId}, replacement={"_id": self.maxId, "title": "Ron"})
-        self.collection.replace_one(filter={"peopleId": self.maxId}, replacement={"peopleId": self.maxId, "maxId": self.maxId + "0"})
+        # self.collection.replace_one(filter={"peopleId": self.maxId}, replacement={"peopleId": self.maxId, "maxId": self.maxId + "0"})
 
-
-
+        # beginTime = time.time()
+        # countFail = 0
+        # countSuccess = 0
+        # for x in range(100000):
+        #     if x / 10000 == 0:
+        #         print("x ==> ", x)
+        #     dic = self.createRegisterJson()
+        #     try:
+        #         self.collection.insert_one(dic)
+        #         countSuccess += 1
+        #     except:
+        #         # traceback.print_exc()
+        #         countFail += 1
+        #
+        #
+        # print("countSuccess ===> ", countSuccess, " countFail ==> ", countFail)
+        # print("costTime ==> ", time.time() - beginTime)
 
 
 
